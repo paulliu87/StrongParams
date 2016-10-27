97d18c238cbaef0e176e76f0b977ec5998c7258d
Paul Liu & Warner Nielsen <pair+paulliu87.warniel08@devbootcamp.com>
2016-10-26 19:50:44 -0700

    Comment the strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    # @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.create(params[:comment])
     redirect_to article_path(@article)
   end
 
